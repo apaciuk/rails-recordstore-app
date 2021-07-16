cac3e4c3e6ebf09379161104d93e8cbcdeddec8d
Paul Anthony McGowan <xhostcom@gmail.com>
2021-07-16 10:23:24 +0100

    Add seed data return json

--- db/schema.rb
+++ db/schema.rb
@@ -47,10 +47,8 @@ ActiveRecord::Schema.define(version: 2021_07_15_201653) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
-    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_artists_on_user_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -126,7 +124,6 @@ ActiveRecord::Schema.define(version: 2021_07_15_201653) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "artists", "users"
   add_foreign_key "records", "artists"
   add_foreign_key "records", "users"
   add_foreign_key "services", "users"
